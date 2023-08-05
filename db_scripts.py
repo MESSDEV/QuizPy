7e94334987bbafa88190db2ddb05aa6e9910c796
Ali Messaoudi <83890004+MESSDEV@users.noreply.github.com>
2023-08-05 12:38:05 +0200

    Update db_scripts.py

--- db_scripts.py
+++ db_scripts.py
@@ -1,35 +1,130 @@
-from random import randint
-from flask import Flask, session, redirect, url_for
-from db_scripts import get_question_after
-quiz = 0
-last_question = 0
-def index():
-   global quiz, last_question
-   max_quiz = 3
-   # or if the student wrote get_quiz_count(), then you can import it and specify:
-   # max_quiz = get_quiz_count[0] 
-   quiz = randint(1, max_quiz)
-   # or if the student wrote get_quiz_count(), then you can import it and specify:
-   # session['quiz'] = get_random_quiz_id()
-   last_question = 0
-   return '<a href="/test">Test</a>'
-def test():
-   global last_question
-   result = get_question_after(last_question, quiz)
-   if result is None or len(result) == 0:   
-           return redirect(url_for('result'))
+import sqlite3
+db_name = 'quiz.sqlite'
+conn = None
+cursor = None
  
-   else:
-       last_question = result[0]
-       # if we've taught the database to return Row or dict, then we shouldn't write result[0] and instead write result['id']
-       return '<h1>' + str(quiz) + '<br>' + str(result) + '</h1>'
-def result():
-   return "that's all folks!"
-# Creating a web application object:
-app = Flask(__name__)  
-app.add_url_rule('/', 'index', index) # creates a rule for the URL '/'
-app.add_url_rule('/test', 'test', test) # creates a rule for the URL '/test'
-app.add_url_rule('/result', 'result', result) # creates a rule for the URL '/test'
-if __name__ == '__main__':
-   # Starting the web server:
-   app.run()
+def open():
+    global conn, cursor
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+ 
+def close():
+    cursor.close()
+    conn.close()
+ 
+def do(query):
+    cursor.execute(query)
+    conn.commit()
+ 
+def clear_db():
+    ''' deletes all tables '''
+    open()
+    query = '''DROP TABLE IF EXISTS quiz_content'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS question'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS quiz'''
+    do(query)
+    close()
+    
+def create():
+    open()
+    cursor.execute('''PRAGMA foreign_keys=on''')
+ 
+    do('''CREATE TABLE IF NOT EXISTS quiz (
+           id INTEGER PRIMARY KEY,
+           name VARCHAR)''')
+ 
+    do('''CREATE TABLE IF NOT EXISTS question (
+               id INTEGER PRIMARY KEY,
+               question VARCHAR,
+               answer VARCHAR,
+               wrong1 VARCHAR,
+               wrong2 VARCHAR,
+               wrong3 VARCHAR)''')
+ 
+    do('''CREATE TABLE IF NOT EXISTS quiz_content (
+               id INTEGER PRIMARY KEY,
+               quiz_id INTEGER,
+               question_id INTEGER,
+               FOREIGN KEY (quiz_id) REFERENCES quiz (id),
+               FOREIGN KEY (question_id) REFERENCES question (id) )''')
+    close()
+ 
+def add_questions():
+    questions = [
+        ('How many months in a year have 28 days?', 'All', 'One', 'None','Two'),
+        ('What will the green cliff look like if it falls into the Red Sea?', 'Wet', 'Red', 'Will not change', 'Purple'),
+        ('Which hand is better to stir tea with?', 'With a spoon', 'Right', 'Left', 'Any'),
+        ('What has no length, depth, width, or height, but can be measured?', 'Time', 'Stupidity', 'The sea','Air'),
+        ('When is it possible to draw out water with a net?', 'When the water is frozen', 'When there are no fish', 'When the goldfish swim away', 'When the ne breaks'),
+        ('What is bigger than an elephant and weighs nothing?', 'Shadow of elephant','A balloon','A parachute', 'A cloud')
+    ]
+    open()
+    cursor.executemany('''INSERT INTO question (question, answer, wrong1, wrong2, wrong3) VALUES (?,?,?,?,?)''', questions)
+    conn.commit()
+    close()
+ 
+def add_quiz():
+    quizes = [
+        ('Own game', ),
+        ('Who wants to be a millionaire?', ),
+        ('The smartest', )
+    ]
+    open()
+    cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''', quizes)
+    conn.commit()
+    close()
+ 
+def add_links():
+    open()
+    cursor.execute('''PRAGMA foreign_keys=on''')
+    query = "INSERT INTO quiz_content (quiz_id, question_id) VALUES (?,?)"
+    answer = input("Add a link (y/n)?")
+    while answer != 'n':
+        quiz_id = int(input("quiz id: "))
+        question_id = int(input("question id: "))
+        cursor.execute(query, [quiz_id, question_id])
+        conn.commit()
+        answer = input("Add a link (y/n)?")
+    close()
+ 
+def show(table):
+    query = 'SELECT * FROM ' + table
+    open()
+    cursor.execute(query)
+    print(cursor.fetchall())
+    close()
+ 
+def show_tables():
+    show('question')
+    show('quiz')
+    show('quiz_content')
+ 
+def get_question_after(question_id = 0, quiz_id=1):
+    ''' returns the next question after the question with the passed ID
+    for the first question, the default value is passed'''
+    open()
+    query = '''
+    SELECT quiz_content.id, question.question, question.answer, question.wrong1, question.wrong2, question.wrong3
+    FROM question, quiz_content
+    WHERE quiz_content.question_id == question.id
+    AND quiz_content.id > ? AND quiz_content.quiz_id == ?
+    ORDER BY quiz_content.id '''
+    cursor.execute(query, [question_id, quiz_id] )
+    result = cursor.fetchone()
+    close()
+    return result
+ 
+def main():
+    clear_db()
+    create()
+    add_questions()
+    add_quiz()
+    add_links()
+    show_tables()
+    # Output to the console of a question with id=3, quiz id = 1
+    print(get_question_after(3, 1))
+ 
+if __name__ == "__main__":
+    main()
